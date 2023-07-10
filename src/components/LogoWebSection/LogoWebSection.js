bdf591d4e29f6e50c00d8ef2981e92fb15a79e08
Yon Valencion <herokiller32e@gmail.com>
2023-07-09 20:08:43 -0700

    Image fixes

--- src/components/LogoWebSection/LogoWebSection.js
+++ src/components/LogoWebSection/LogoWebSection.js
@@ -7,25 +7,25 @@ const LogoWebSection = () => {
             <p className={styles.desc}>CryptoSecure's global payment platform is widely integrated and accepted at over 15,000 mercents. Rest easy knowing you money is good anywhere on the globe and online.</p>
             <div className={styles.web}>
                 <div className={styles["row-1"]}>
-                    <img className={styles["web-image"]} src="web-images/bank-logo-1.png" />
-                    <img className={styles["web-image"]} src="web-images/adobe-logo.png" />
-                    <img className={styles["web-image"]} src="web-images/robinhood-logo.png" />
-                    <img className={styles["web-image"]} src="web-images/apple-logo.png" />
-                    <img className={styles["web-image"]} src="web-images/chase-logo.png" />
-                    <img className={styles["web-image"]} src="web-images/airbnb-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/bank-logo-1.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/adobe-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/robinhood-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/apple-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/chase-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/airbnb-logo.png" />
                 </div>
                 <div className={styles["row-2"]}>
-                    <img className={styles["web-image"]} src="web-images/netflix-logo.png" />
-                    <img className={styles["web-image"]} src="web-images/paypal-logo.png" />
-                    <img className={styles["web-image"]} src="web-images/coinbase-logo.png" />
-                    <img className={styles["web-image"]} src="web-images/uber-logo.png" />
-                    <img className={styles["web-image"]} src="web-images/spotify-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/netflix-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/paypal-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/coinbase-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/uber-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/spotify-logo.png" />
                 </div>
                 <div className={styles["row-3"]}>
-                    <img className={styles["web-image"]} src="web-images/zillow-logo.png" />
-                    <img className={styles["web-image"]} src="web-images/citi-logo.png" />
-                    <img className={styles["web-image"]} src="web-images/expedia-logo.png" />
-                    <img className={styles["web-image"]} src="web-images/linked-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/zillow-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/citi-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/expedia-logo.png" />
+                    <img alt="" className={styles["web-image"]} src="web-images/linked-logo.png" />
                 </div>
             </div>
         </div>
