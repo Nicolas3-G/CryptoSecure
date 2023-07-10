6ab496d4b3172db99c5ef4265bc9151768320341
Yon Valencion <herokiller32e@gmail.com>
2023-07-09 19:58:48 -0700

    deployment changes

--- src/components/IntroSection/IntroSection.js
+++ src/components/IntroSection/IntroSection.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "./IntroSection.module.css";
 
 const IntroSection = () => {
@@ -6,7 +7,7 @@ const IntroSection = () => {
             <div className={styles["left-side"]}>
                 <h1 className={styles.title}>Effortless Crypto Banking Experience</h1>
                 <p className={styles["desc-text"]}>At CryptoSecure Financial, we believe in the power of cryptocurrency to transform the financial industry. Our mission is to provide a secure, easy-to-use platform that empowers individuals and businesses to manage their digital assets with confidence.</p>
-                <a href="/checking"><button className={styles["button-1"]}>Learn More</button></a>
+                <Link href="/checking"><button className={styles["button-1"]}>Learn More</button></Link>
                 <button className={styles["button-2"]}>Create Account</button>
             </div>
             <div className={styles["right-side"]}>
